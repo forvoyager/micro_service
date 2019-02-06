43c660e84d646c4333587b03f7edeff3d014c73d
Arnold Yang <forvoyager@outlook.com>
2019-02-06 08:50:17 +0800

    api操作定义

---
+++ ms-account/ms-account-service/src/main/java/com/xr/account/AccountServiceConfiguration.java
@@ -0,0 +1,12 @@
+package com.xr.account;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <b>description</b>：账户服务配置 <br>
+ * <b>author</b>：yangchangyan@yijiedai.com
+ * <b>time</b>：2019-02-01 11:41 <br>
+ */
+@Configuration
+public class AccountServiceConfiguration {
+}
